39ee647af99638e1b616d6b91fa530058fad3a39
Alexis <alecabj12@gmail.com>
2020-10-23 16:02:03 -0500

    contador caracteres

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    value: "",
+    number:0
+  };
+
+  counter = (event) => {
+    this.setState({
+      value: event.target.value,
+      number:event.target.value.length,
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.value} onChange={this.counter.bind(this)} ></textarea>
+        <div className="counter">{this.state.number}</div>
       </div>
     );
   }
